a3cbb66eccb9c053c4f8993c7e91650bca3561dc
uShawnTS <vibarrientosp@gmail.com>
2022-09-25 10:25:45 -0500

    Changes in Lualine configuration

--- lua/shawn/plugin/lualine.lua
+++ lua/shawn/plugin/lualine.lua
@@ -1,36 +1,42 @@
 local status, lualine = pcall(require, 'lualine')
 if not status then return end
 
-lualine.setup {
+lualine.setup({
     options = {
-	icons_enabled = true,
-	theme = 'solarized',
-	component_separators = { left = '', right = ''},
-	section_separators = { left = '', right = ''},
-	always_divide_middle = true,
-	globalstatus = true,
-	refresh = {
-	    statusline = 1000,
-	    tabline = 1000,
-	    winbar = 1000
-	}
+        theme = 'solarized_dark',
+        component_separators = '|',
+        section_separators = '',
+        icons_enabled = true,
+        globalstatus = false,
+	disabled_filetypes = {},
     },
     sections = {
-	lualine_a = {'mode'},
-	lualine_b = {'branch', 'diff', 'diagnostics'},
-	lualine_c = {'filename'},
-	lualine_x = {'encoding', 'fileformat', 'filetype'},
-	lualine_y = {'progress'},
-	lualine_z = {'location'}
+        lualine_a = { 'mode' },
+        lualine_b = {
+	    { 'branch' },
+	    { 'diff', colored = false }
+	},
+        lualine_c = {
+            { 'filename', file_status = true, path = 0 }, -- just filename, 1 = relative path, 2 = absolute path
+            { 'diagnostics' },
+        },
+        lualine_x = {
+	    { 'diagnostics', sources = { 'nvim_diagnostic' }, symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' } },
+	    'filetype'
+	},
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
     },
     inactive_sections = {
-	lualine_c = {'filename'},
-	lualine_x = {'location'}
+	lualine_a = {},
+	lualine_b = {},
+	lualine_c = { 'filename' },
+	lualine_x = {},
+	lualine_y = {},
+	lualine_z = { 'location' },
     },
     tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {}
-}
+    extensions = {},
+})
 
 lualine.get_config()
