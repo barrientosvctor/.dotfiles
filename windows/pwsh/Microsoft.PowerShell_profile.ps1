b373bf18855876a6d61e4d6a93babeff3ec33eed
barrientosvctor <vibarrientosp@gmail.com>
2022-11-27 10:49:39 -0500

    Alias added

--- windows/pwsh/Microsoft.PowerShell_profile.ps1
+++ windows/pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,14 @@
-# Set UTF-8 on PowerShell
-[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
-
 # Oh my posh
 # oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\shawn.omp.json" | Invoke-Expression
 # oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\shawn-solarized.omp.json" | Invoke-Expression
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\shawn-half-life.omp.json" | Invoke-Expression
-
-# Icons
-Import-Module Terminal-Icons
 
 # PSReadLine (Terminal autocompletation)
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineOption -PredictionSource History
+
+# Alias
+Set-Alias ll ls
+Set-Alias g git
