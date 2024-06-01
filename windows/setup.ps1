e6ebab07a748671e37defe251f07651eb280346b
barrientosvctor <vibarrientosp@gmail.com>
2024-05-31 23:24:06 -0500

    Refactor powershell setup script

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,9 +1,31 @@
 Param(
+    # Target passed as input.
+    ##! Modify the help message when a new target is created.
+    [Parameter(Mandatory, HelpMessage = 'Targets: all, help, modules, alacritty, symlink' )]
     [string]$Target
 )
 
-$Targets = @('help', 'modules', 'alacritty', 'symlink', 'all')
-$ListOfTargets = $Targets.foreach({"`n-> $PSItem"})
+##! Modify this hash table every time a new target is needed to create.
+# 'targetName' = 'FunctionName'
+# When the input matches with some key in the hash table, the function name passed as value will be invoke.
+$hashTableTargets = @{
+    'all' = "PS_InvokeAllTargets";
+    'help' = "PS_ShowScriptInfo";
+    'modules' = "PS_InstallModules";
+    'alacritty' = "PS_SetupAlacrittyConfigFile";
+    'symlink' = "PS_SetupSymlinks";
+}
+
+# Formats the hash table to a comprehensible string
+$availableTargets = $hashTableTargets.Keys.ForEach({"`n-> $PSItem"})
+
+# Used to when `all` target is matched.
+##! Modify it when a new target function is created.
+function PS_InvokeAllTargets {
+    PS_InstallModules
+    PS_SetupAlacrittyConfigFile
+    PS_SetupSymlinks
+}
 
 function PS_InstallModules {
     Write-Host "Installing PSReadLine..."
@@ -30,20 +52,14 @@ function PS_SetupSymlinks {
     New-Item -Path "$env:HOMEPATH\.gitconfig" -ItemType SymbolicLink -Value "$PWD\.gitconfig"
 }
 
-switch -Exact ($Target) {
-    { $_ -eq "help" } {
-        Write-Host "Usage: .\setup.ps1 -Target <target>"
-        Write-Host "Available targets: $ListOfTargets"
-    }
-    { $_ -eq "modules" } { PS_InstallModules }
-    { $_ -eq "alacritty" } { PS_SetupAlacrittyConfigFile }
-    { $_ -eq "symlink" } { PS_SetupSymlinks }
-    { $_ -eq "all" } {
-        PS_InstallModules
-        PS_SetupAlacrittyConfigFile
-        PS_SetupSymlinks
-    }
-    Default {
-        Write-Host "Unrecognized target. Available targets: $ListOfTargets"
-    }
+# Used to when `help` target is matched.
+function PS_ShowScriptInfo {
+    Write-Host "Usage: .\setup.ps1 -Target <target>"
+    Write-Host "Available targets: $availableTargets"
+}
+
+if ($hashTableTargets.ContainsKey($Target)) {
+    Invoke-Expression -Command $hashTableTargets[$Target]
+} else {
+    Write-Host "Unrecognized target. Available targets: $availableTargets"
 }
