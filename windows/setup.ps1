914df6f13dae6c987e2026af7298d2bcd86d5679
barrientosvctor <vibarrientosp@gmail.com>
2024-06-01 10:59:24 -0500

    Add function about amount of changes
    The function will used for every target

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -5,6 +5,8 @@ Param(
     [string]$Target
 )
 
+[int] $processCount = 0
+
 ##! Modify this hash table every time a new target is needed to create.
 # 'targetName' = 'FunctionName'
 # When the input matches with some key in the hash table, the function name passed as value will be invoke.
@@ -27,9 +29,30 @@ function PS_InvokeAllTargets {
     PS_SetupSymlinks
 }
 
+# Used to show the amount of changes made in a target.
+function PS_CountChanges {
+    param(
+        [Parameter(Mandatory)]
+        [int]$Count,
+
+        [Parameter(Mandatory)]
+        [string]$ProcessName
+     )
+
+    if ($Count -eq 0) {
+        Write-Host "--> There were no changes in $ProcessName process. <--"
+    } elseif ($Count -gt 0) {
+        Write-Host "--> $ProcessName process successfully executed with $Count changes. <--"
+    } else {
+        Write-Host "--> $ProcessName process successfully executed with unknown number of changes. <--"
+    }
+}
+
 function PS_InstallModules {
     Write-Host "Installing PSReadLine..."
     Install-Module PSReadLine
+    Install-Module "--> PSReadLine installed."
+    PS_CountChanges -Count -1 -ProcessName "Modules"
 }
 
 # This function assumes you're located in dotfiles's root directory
@@ -40,12 +63,17 @@ function PS_SetupAlacrittyConfigFile {
     if (-Not (Test-Path -Path $Alacritty_Path)) {
         Write-Host "'$Alacritty_Path' not found. Creating..."
         New-Item -ItemType Directory -Path $Alacritty_Path
+        $processCount = $processCount + 1
     }
 
     if (-Not (Test-Path -Path "$Alacritty_Path\alacritty.toml")) {
         Write-Host "'$Alacritty_File' not found. Creating a symlink..."
         New-Item -Path $Alacritty_File -ItemType SymbolicLink -Value "$PWD\.config\alacritty\alacritty.toml"
+        $processCount = $processCount + 1
     }
+
+    PS_CountChanges -Count $processCount -ProcessName "Alacritty"
+    $processCount = 0
 }
 
 function PS_SetupSymlinks {
