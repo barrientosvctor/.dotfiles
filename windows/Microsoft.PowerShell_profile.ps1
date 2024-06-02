e2097847d27fdd22e7653087d4ce4ac29734877f
barrientosvctor <vibarrientosp@gmail.com>
2024-06-02 14:52:38 -0500

    Add functions and aliases in pwsh config

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,26 @@
+function which ($command) {
+    Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+function admin {
+    if ($PSVersionTable.PSEdition -eq "Core") {
+        $powershell_cmd = "pwsh.exe"
+    } elseif ($PSVersionTable.PSEdition -eq "Desktop") {
+        $powershell_cmd = "powershell.exe"
+    }
+
+    [string] $command = $args -join " "
+
+    if ($args.Count -gt 0) {
+        Start-Process "$PSHOME\$powershell_cmd" -Verb RunAs -ArgumentList "-NoLogo","-Interactive","-Command","$command; pause"
+    } else {
+        Start-Process "$PSHOME\$powershell_cmd" -Verb RunAs
+    }
+}
+
+# ======= MODULE IMPORTS =======
+
 # If it's running on powershell not core, with version less than 7 and exists a module named 'PSReadLine' installed.
 if (-not ($PSVersionTable.PSEdition -eq "Core" -and $PSVersionTable.PSVersion.Major -ge 7 -and $PSVersionTable.PSVersion.Minor -ge 4)) {
     if ($null -ne (Get-Module -Name PSReadLine)) {
@@ -5,6 +28,7 @@ if (-not ($PSVersionTable.PSEdition -eq "Core" -and $PSVersionTable.PSVersion.Ma
     }
 }
 
+# ======= MODULE CONFIG =======
 # PSReadLine (Terminal autocompletation)
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
@@ -12,12 +36,10 @@ Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 
-# Alias
+# ======= ALIASES =======
 Set-Alias -Name l -Value ls
 Set-Alias -Name g -Value git
 Set-Alias -Name v -Value nvim
-
-function which ($command) {
-  Get-Command -Name $command -ErrorAction SilentlyContinue |
-    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
-}
+Set-Alias -Name df -Value Get-Volume
+Set-Alias -Name sudo -Value admin
+Set-Alias -Name su -Value admin
