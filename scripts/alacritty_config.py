f25c4165f30660161e3054af7301c65953cc7b07
barrientosvctor <vibarrientosp@gmail.com>
2024-06-03 22:58:17 -0500

    Add script to setup alacritty shell

---
+++ scripts/alacritty_config.py
@@ -0,0 +1,93 @@
+from os import getenv, path, getcwd
+from platform import system
+
+available_os = ["windows", "unix"]
+available_shells = [
+    ("WSL", "Pwsh", "Powershell"), # Windows
+    ("bash", "zsh"), # Unix
+]
+
+shell_cmd = [
+    ("wsl", "pwsh", "powershell"), #Windows
+    ("/bin/bash", "/bin/zsh"), #Unix
+]
+wsl_root: str = "~"
+
+if system() == "Windows":
+    win_root = getenv("HOMEPATH")
+
+    if win_root:
+        wsl_root = "/mnt/c/{}".format(win_root.replace('\\', '/'))
+
+alacritty_terminal_args = {
+    # Windows
+    0: {
+        0: ("wsl.exe", "--cd", wsl_root),
+        1: ("-NoLogo", "-NoExit", "-Command", "cd ~"),
+        2: ("-NoLogo", "-NoExit", "-Command", "cd ~"),
+    },
+    # Unix
+    1: {
+        # Bash
+        0: ("-c", "cd ~"),
+        # Zsh
+        1: ("-c", "cd ~")
+    },
+}
+
+def list_available_os():
+    for num_option in range(0, len(available_os)):
+        print(f"{num_option}. {available_os[num_option]}")
+        num_option += 1
+
+def list_available_shell_by_os(num_os):
+    for shell_option in range(0, len(available_shells[num_os])):
+        print(f"{shell_option}. ${available_shells[num_os][shell_option]}")
+        shell_option += 1
+
+
+def make_toml_array(tpl: tuple):
+    array_literal = '['
+
+    for i in range(0, tpl.__len__()):
+        if i+1 >= tpl.__len__():
+            array_literal += f'"{tpl[i]}"]'
+        else:
+            array_literal += f'"{tpl[i]}",'
+
+    return array_literal
+
+def alacritty():
+    chosen_terminal_args = tuple(alacritty_terminal_args[os_option][shell_option])
+    args = make_toml_array(chosen_terminal_args)
+    alacritty_config_path = path.join(getcwd(), ".config", "alacritty", "alacritty.toml")
+
+    with open(alacritty_config_path, "a+") as f:
+        if f.writable():
+            f.write("\n")
+            f.write("[shell]\n")
+            f.write(f'program = "{shell_cmd[os_option][shell_option]}"\n')
+            f.write("args = " + args + '\n')
+
+        print(f.read())
+
+        if not f.closed:
+            f.close()
+
+list_available_os()
+
+os_option = int(input("Choose the operative system you are using: "))
+
+while os_option < 0 or os_option > len(available_os) - 1:
+    list_available_os()
+    os_option = int(input("That option doesn't exists, choose another: "))
+
+list_available_shell_by_os(os_option)
+
+shell_option = int(input("Type the shell you want to use for Alacritty: "))
+
+while shell_option < 0 or shell_option > len(available_shells[os_option]):
+    list_available_shell_by_os(os_option)
+    shell_option = int(input("That option doesn't exists, choose another: "))
+
+alacritty()
