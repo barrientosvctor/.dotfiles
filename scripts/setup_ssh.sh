54c978d78d3b2188e8057b0c89ee5c2aae5bdf2f
barrientosvctor <vibarrientosp@gmail.com>
2024-06-12 12:27:29 -0500

    Add ssh key setup

---
+++ scripts/setup_ssh.sh
@@ -0,0 +1,20 @@
+# in case you want to run the script on windows, run it inside Git Bash
+echo "Type the email you will associate with ssh and git: "
+read email
+
+echo "Give a name for the resulting ssh key file (otherwise will be named as ed25519): "
+read keyname
+
+if [[ $keyname -eq "" ]];
+then
+    $keyname="ed25519"
+fi
+
+private_ssh_keypath="$HOME/.ssh/$keyname"
+
+ssh-keygen -t ed25519 -C $email -f $private_ssh_keypath
+eval "$(ssh-agent -s)"
+ssh-add $private_ssh_keypath
+
+echo "SSH key added at '$private_ssh_keypath'."
+echo "Now, add your public ssh key to the website destiny (github, gitlab, bitbucket)."
